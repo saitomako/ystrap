789895cbb8fa59373aa1b64a716022b024e2bdca
Makoto Saito <saitomako@gmail.com>
2019-05-11 15:28:03 +0900

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+// Defining requirements
+var gulp = require('gulp');
+var plumber = require('gulp-plumber');
+var sass = require('gulp-sass');
+var babel = require('gulp-babel');
+var postcss = require('gulp-postcss');
+var watch = require('gulp-watch');
+var rename = require('gulp-rename');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin');
+var ignore = require('gulp-ignore');
+var rimraf = require('gulp-rimraf');
+var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require('browser-sync').create();
+var del = require('del');
+var cleanCSS = require('gulp-clean-css');
+var gulpSequence = require('gulp-sequence');
+var replace = require('gulp-replace');
+var autoprefixer = require('autoprefixer');
+
+// Configuration file to keep your code DRY
+var cfg = require('./gulpconfig.json');
+
